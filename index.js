b1cd6542dfd2825cd8d73ae4ce10a31ffd3fc3c7
Heloisa Cardillo Lima <heloisacardillo@gmail.com>
2025-09-03 14:11:26 -0300

    <docs> RI3 finalizada

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-
 class Cliente {
     #cpf
-    constructor (nome, sobrenome, cpf, telefone, endereco) {
+    constructor(nome, sobrenome, cpf, endereco, telefone) {
         this.nome = nome
         this.sobrenome = sobrenome
+        this.#cpf = cpf
         this.telefone = telefone
         this.endereco = endereco
-        this.#cpf = cpf
-        this.telefone = new Set()
-        this.endereco = new Set()
 
-        this.getNome = function() { return this.nome; };
-        this.setNome = function(nome) { this.nome = nome; };
+        this.getNome = function() { return this.nome }
+        this.setNome = function(nome) { this.nome = nome }
 
-        this.getNomeMaiusculo = function() { return this.nome.toUpperCase(); };
-        this.getNomeMinusculo = function() { return this.nome.toLowerCase(); };
+        this.getNomeMaiusculo = function() { return this.nome.toUpperCase() }
+        this.getNomeMinusculo = function() { return this.nome.toLowerCase() }
 
-         this.getDados = function () { 
-        return "Cliente: " + this.nome + this.sobrenome +
-        "\nTelefone: " + (this.telefone.getDdd ? this.telefone.getNumeroTelefone() : "Não informado") +
-        "\nEndereço: " + this.endereco.getEstado() + ", " + this.endereco.getCidade() + this.endereco.getRua() + this.endereco.getNumero();
-    };
+        this.getSobrenomeMaiusculo = function() { return this.sobrenome.toUpperCase() }
+        this.getSobrenomeMinusculo = function() { return this.sobrenome.toLowerCase() }
 
+        this.getCpf = function() { return this.#cpf }
     }
 
+    getDados() {
+        return "Cliente: " + this.nome + ", " + this.sobrenome +
+        "\nCPF: " + this.getCpf() +
+        "\nTelefone: (" + this.telefone.getDdd() + ") " + this.telefone.getNumeroTelefone() +
+        "\nEndereço: " + this.endereco.getRua() + ", " + this.endereco.getNumero() + " - " +
+        this.endereco.getCidade() + " / " + this.endereco.getEstado()
+    }
 }
 
 class Telefone {
@@ -31,86 +33,89 @@ class Telefone {
         this.ddd = ddd
         this.numeroTelefone = numeroTelefone
 
-        this.getDdd = function() { return this.ddd; };
-        this.setDdd = function(ddd) { this.ddd = ddd; };
+        this.getDdd = function() { return this.ddd }
+        this.setDdd = function(ddd) { this.ddd = ddd }
 
-        this.getNumeroTelefone = function() { return this.numeroTelefone; };
-        this.setNumeroTelefone = function(numeroTelefone) { this.numeroTelefone = numeroTelefone; };
-
+        this.getNumeroTelefone = function() { return this.numeroTelefone }
+        this.setNumeroTelefone = function(numeroTelefone) { this.numeroTelefone = numeroTelefone }
     }
 }
 
-class Endereco{
-    constructor (estado, cidade, rua, numero){
+class Endereco {
+    constructor(estado, cidade, rua, numero) {
         this.estado = estado
         this.cidade = cidade
         this.rua = rua
         this.numero = numero
 
-    this.getRua = function() { return this.rua; };
-    this.setRua = function(rua) { this.rua = rua; };
+        this.getRua = function() { return this.rua }
+        this.setRua = function(rua) { this.rua = rua }
 
-    this.getRuaMaiusculo = function() { return this.rua.toUpperCase(); };
-    this.getRuaMinusculo = function() { return this.rua.toLowerCase(); };
+        this.getRuaMaiusculo = function() { return this.rua.toUpperCase() }
+        this.getRuaMinusculo = function() { return this.rua.toLowerCase() }
 
-    this.getCidade = function() { return this.cidade; };
-    this.setCidade = function(cidade) { this.cidade = cidade; };
+        this.getCidade = function() { return this.cidade }
+        this.setCidade = function(cidade) { this.cidade = cidade }
 
-    this.getCidadeMaiusculo = function() { return this.cidade.toUpperCase(); };
-    this.getCidadeMinusculo = function() { return this.cidade.toLowerCase(); };
+        this.getCidadeMaiusculo = function() { return this.cidade.toUpperCase() }
+        this.getCidadeMinusculo = function() { return this.cidade.toLowerCase() }
 
-    this.getEstado = function() { return this.estado; };
-    this.setEstado = function(estado) { this.estado = estado; };
+        this.getEstado = function() { return this.estado }
+        this.setEstado = function(estado) { this.estado = estado }
 
-    this.getNumero = function() { return this.numero; };
-    this.setNumero = function(numero) { this.numero = numero; };
-
+        this.getNumero = function() { return this.numero }
+        this.setNumero = function(numero) { this.numero = numero }
     }
 }
 
 class Empresa {
     #cnpj
-    constructor (razaoSocial, nomeFantasia, cnpj) {
+    constructor(razaoSocial, nomeFantasia, cnpj) {
         this.razaoSocial = razaoSocial
         this.nomeFantasia = nomeFantasia
         this.#cnpj = cnpj
+        this.clientes = []
+    }
+
+    getCnpj() { return this.#cnpj }
+
+    adicionarCliente(cliente) {
+        this.clientes.push(cliente)
+    }
+
+    getDados() {
+        let dados = `Empresa: ${this.razaoSocial} (${this.nomeFantasia})\nCNPJ: ${this.getCnpj()}`
+        dados += `\n\n--------------------------------\n\n`
+        let clientesOrdenados = ordenarClientesPorNome(this.clientes)
+        clientesOrdenados.forEach(c => {
+            dados += c.getDados() + "\n\n"
+        })
+        return dados
     }
 }
 
-var empresa = new Empresa ("Coelhos LTDA", "Casa dos Coelhos", '00000000')
+var empresa = new Empresa("Coelhos LTDA", "Casa dos Coelhos", '00000000')
 
-var cli1 = new Cliente ("José", "O coelho gordo", 10101010, Endereco)
-var end1 = new Endereco ("SP", "São José dos Campos", "Engenheiro Jose Longo", 622)
-var tel1 = new Telefone (12, 11111111)
+var cli1 = new Cliente("José", "O coelho gordo", 10101010, new Endereco("SP", "São José dos Campos", "Engenheiro Jose Longo", 622), new Telefone(12, 11111111))
+var cli2 = new Cliente("Dani", "A coelha medrosa", 12121212, new Endereco("SP", "São José dos Campos", "Engenheiro Jose Longo", 622), new Telefone(12, 22222222))
+var cli3 = new Cliente("Amy", "A coelha exploradora", 13131313, new Endereco("SP", "São José dos Campos", "Engenheiro Jose Longo", 622), new Telefone(12, 33333333))
+var cli4 = new Cliente("Frida", "A coelha destemida", 14141414, new Endereco("SP", "São José dos Campos", "Engenheiro Jose Longo", 622), new Telefone(12, 4444444))
+var cli5 = new Cliente("Hanna", "O coelho curioso", 15151515, new Endereco("SP", "São José dos Campos", "Engenheiro Jose Longo", 622), new Telefone(12, 5555555))
 
-var cli2 = new Cliente ("Dani", "A coelha medrosa", 12121212, Endereco)
-var end2 = new Endereco ("SP", "São José dos Campos", "Engenheiro Jose Longo", 622)
-var tel2 = new Telefone (12, 22222222)
 
-var cli3 = new Cliente ("Amy", "A coelha exploradora", 13131313, Endereco)
-var end3 = new Endereco ("SP", "São José dos Campos", "Engenheiro Jose Longo", 622)
-var tel3 = new Telefone (12, 33333333)
+empresa.adicionarCliente(cli1)
+empresa.adicionarCliente(cli2)
+empresa.adicionarCliente(cli3)
+empresa.adicionarCliente(cli4)
+empresa.adicionarCliente(cli5)
 
-var cli4 = new Cliente ("Frida", "A coelha destemida", 14141414, Endereco)
-var end4 = new Endereco ("SP", "São José dos Campos", "Engenheiro Jose Longo", 622)
-var tel4 = new Telefone (12, 4444444)
-
-var cli5 = new Cliente ("Hanna", "O coelho curioso", 15151515, Endereco)
-var end5 = new Endereco ("SP", "São José dos Campos", "Engenheiro Jose Longo", 622)
-var tel5 = new Telefone (12, 5555555)
-
-//Coloquei para ordenar pelo nome dos clientes
-
+//Coloquei para ordenar os clientes por ordem alfabética dos nomes
 function ordenarClientesPorNome(clientes) {
     return clientes.slice().sort(function(a, b) {
         return a.getNome().localeCompare(b.getNome());
     });
 }
 
-var listaClientes = [cli1, cli2, cli3, cli4, cli5];
-var listaOrdenada = ordenarClientesPorNome(listaClientes);
+console.log(empresa.getDados())
 
-listaOrdenada.forEach(function(cliente) {
-    console.log(cliente.getDados());
-    console.log("\n");
-});
+
